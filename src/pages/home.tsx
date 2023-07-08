e14bcd1e7c4504ebaecc80d21815f1f8966aad0a
Enijar <TheEnijar@gmail.com>
2023-07-08 10:44:49 +0100

    chore: set up three.js

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <Canvas flat linear gl={{ alpha: false }}>
+      <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+      <OrbitControls />
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial color="crimson" />
+      </mesh>
+    </Canvas>
   );
 }
