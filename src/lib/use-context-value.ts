2b029ab05aeb15ce6bd173ddd46e255a888b7b05
Enijar <TheEnijar@gmail.com>
2023-08-13 10:56:55 +0100

    chore: clean up

--- src/lib/use-context-value.ts
+++ src/lib/use-context-value.ts
@@ -2,12 +2,11 @@ import React from "react";
 import type { LayerContextType } from "@/lib/context";
 import type { ContainerSize, StyleProps } from "@/lib/types";
 import { generateUniqueId } from "@/lib/utils";
-import { LayerContext } from "@/lib/context";
 
 export default function useContextValue(children: React.ReactElement[], style: StyleProps, size: ContainerSize) {
   const id = React.useMemo(() => generateUniqueId(), []);
   return React.useMemo<LayerContextType>(() => {
-    const overflow = style.overflow ?? "visible";
+    const overflow = style.overflow;
     return {
       id,
       parent: { size, overflow },
