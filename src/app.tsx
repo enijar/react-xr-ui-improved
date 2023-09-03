0c4d8c4e68deb7c6ea6a1ee2e1439fc90ef3c565
Enijar <TheEnijar@gmail.com>
2023-09-03 11:27:22 +0100

    chore: update example with animation

--- src/app.tsx
+++ src/app.tsx
@@ -5,15 +5,18 @@ import Layer, { LayerRef } from "@/lib/layer";
 export default function App() {
   const layerRef = React.useRef<LayerRef>(null);
 
+  const [borderRadius, setBorderRadius] = React.useState(0);
+
   useFrame(() => {
     const layer = layerRef.current;
     if (layer === null) return;
     if (layer.group === null) return;
     const progress = Date.now();
-    const speed = 150;
+    const speed = 250;
     const amplitude = 0.25;
     layer.group.position.x = Math.cos(progress / speed) * amplitude;
     layer.group.position.y = Math.sin(progress / speed) * amplitude;
+    setBorderRadius((1 + Math.sin(progress / speed)) / 2);
   });
 
   return (
@@ -54,9 +57,18 @@ export default function App() {
           ref={layerRef}
           width="50%"
           aspectRatio={1}
-          style={{ backgroundColor: "lightblue", borderRadius: 0.5, alignItems: "center", justifyContent: "center" }}
+          style={{
+            backgroundColor: "lightblue",
+            borderRadius: 0.5 * borderRadius,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          <Layer width="25%" aspectRatio={1} style={{ backgroundColor: "brown" }} />
+          <Layer
+            width="25%"
+            aspectRatio={1}
+            style={{ backgroundColor: "brown", borderRadius: 0.125 * (1 - borderRadius) }}
+          />
         </Layer>
       </Layer>
     </>
